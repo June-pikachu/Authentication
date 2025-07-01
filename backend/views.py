a22f64a27ab323cd4f87c37220952ab928bd294e
June <147446400+June-pikachu@users.noreply.github.com>
2025-07-01 11:16:25 +0300

    Update views.py

--- backend/views.py
+++ backend/views.py
@@ -19,19 +19,6 @@ def signup_view(request):
         form = UserCreationForm()
     return render(request, 'registration/signup.html', {'form': form})
 
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            # Redirect to a success page or home page
-            return redirect('home')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'registration/login.html', {'form': form})
-
-
 class SecureAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
